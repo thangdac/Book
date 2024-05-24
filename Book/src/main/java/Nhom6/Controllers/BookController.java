4d4dfa1d7b1f4bb566bca4cb0273385ad282b6f4
thangdac <nguyendac1606123@gmail.com>
2024-05-24 23:49:50 +0700

    autocomplete

--- Book/src/main/java/Nhom6/Controllers/BookController.java
+++ Book/src/main/java/Nhom6/Controllers/BookController.java
@@ -15,29 +15,11 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @RequestMapping("/books")
 public class BookController {
-
     private final BookService bookService;
-
     @GetMapping
     public String showAllBooks(@NonNull Model model) {
         model.addAttribute("books", bookService.getAllBooks());
         return "book/list";
-    }
-
-    @GetMapping("/search")
-    public String searchBooks(@RequestParam(value = "search", required = false) String searchTerm, Model model) {
-        List<Book> searchResults = bookService.searchBooks(searchTerm);
-        model.addAttribute("books", searchResults);
-        return "book/list";
-    }
-
-    @GetMapping("/autocomplete")
-    @ResponseBody
-    public List<String> autocomplete(@RequestParam("term") String searchTerm) {
-        List<Book> searchResults = bookService.searchBooks(searchTerm);
-        return searchResults.stream()
-                .map(Book::getTitle)
-                .collect(Collectors.toList());
     }
 
     @GetMapping("/add")
@@ -52,24 +34,39 @@ public class BookController {
             bookService.addBook(book);
         return "redirect:/books";
     }
-
     @GetMapping("/edit/{id}")
-    public String editBookForm(@NonNull Model model, @PathVariable long id) {
+    public String editBookForm(@NonNull Model model, @PathVariable long id)
+    {
         var book = bookService.getBookById(id).orElse(null);
         model.addAttribute("book", book != null ? book : new Book());
         return "book/edit";
     }
-
     @PostMapping("/edit")
     public String editBook(@ModelAttribute("book") Book book) {
         bookService.updateBook(book);
         return "redirect:/books";
     }
-
     @GetMapping("/delete/{id}")
     public String deleteBook(@PathVariable long id) {
         if (bookService.getBookById(id).isPresent())
             bookService.deleteBookById(id);
         return "redirect:/books";
+    }
+
+    @GetMapping("/search")
+    public String searchBooks(@RequestParam(value = "search", required = false) String searchTerm,
+                              Model model) {
+        List<Book> searchResults = bookService.searchBooks(searchTerm);
+        model.addAttribute("books", searchResults);
+        return "book/list";
+    }
+    @GetMapping("/autocomplete")
+    @ResponseBody
+    public List<Book> autocomplete(@RequestParam String term) {
+        String lowercaseTerm = term.toLowerCase();
+        return bookService.searchBooks(term).stream()
+                .filter(book -> book.getTitle().toLowerCase().contains(lowercaseTerm)
+                        || book.getAuthor().toLowerCase().contains(lowercaseTerm))
+                .collect(Collectors.toList());
     }
 }
